9f5725251b4849883d7b004baee86cf849d59885
Brenda <bf6819000@gmail.com>
2022-12-28 10:55:47 -0500

    HH

---
+++ JS/script8.js
@@ -0,0 +1,18 @@
+function datos() {
+    var nombre = document.getElementById("nombre").value;
+    console.log(nombre);
+
+    var apellido = document.getElementById("apellido").value;
+    console.log(apellido);
+    
+    var telefono = document.getElementById("telefono").value;
+    console.log(telefono);
+    
+    var email = document.getElementById("email").value;
+    console.log(email);
+
+    alert('Gracias Por Dejarnos tus Datos Nos Estaremos Comunicando Contigo Lo Más Pronto Posible :)');
+    alert('Y Hasta Aquí Llegó Hablando Huevadas Y El Que No Ve Hablando Huevadas Que Me Deje Coja Sino, Next 😜');
+    alert('Porque HH Es Un Arte 🖕');
+}
+
---
+++ JS/script9.js
@@ -0,0 +1,95 @@
+function datosfruta() {
+    var fruta = document.getElementById("fruta").value;
+    switch (fruta) {
+        case 'uva':
+            alert('El precio es de S/. 5.00.');
+            break;
+        case 'fresa':
+            alert('El precio es de S/. 3.00.');
+            break;
+        case 'naranja':
+            alert('El precio es de S/. 3.50.');
+            break;
+        case 'manzana':
+            alert('El precio es de S/ 4.00.');
+            break;
+        case 'plátano':
+        alert('El precio es de S/. 2.00.');
+        break;
+        default:    
+            alert('Tipo de Fruta desconocido.');
+            break;
+    }
+    }
+
+    function datosverdura() {
+        var verdura = document.getElementById("verdura").value;
+        switch (verdura) {
+            case 'apio':
+                alert('El precio es de S/. 3.00.');
+                break;
+            case 'pepino':
+                alert('El precio es de 5 x 6 soles.');
+                break;
+            case 'tomate':
+                alert('El precio es de S/. 2.50.');
+                break;
+            case 'choclo':
+                alert('El precio es de 5 x 8 soles.');
+                break;
+            case 'zanahoria':
+            alert('El precio es de S/. 3.00.');
+            break;
+            default:    
+                alert('Tipo de verdura desconocido.');
+                break;
+        }
+        }
+
+        function datosf() {
+            var fruta = document.getElementById("fruta").value;
+            switch (fruta) {
+                case 'uva':
+                    alert('El precio es de S/. 6.00.');
+                    break;
+                case 'fresa':
+                    alert('El precio es de S/. 5.00.');
+                    break;
+                case 'naranja':
+                    alert('El precio es de S/. 2.50.');
+                    break;
+                case 'manzana':
+                    alert('El precio es de S/ 4.50.');
+                    break;
+                case 'plátano':
+                alert('El precio es de S/. 1.50.');
+                break;
+                default:    
+                    alert('Tipo de Fruta desconocido.');
+                    break;
+            }
+            }
+
+        function datosv() {
+            var verdura = document.getElementById("verdura").value;
+            switch (verdura) {
+                case 'apio':
+                    alert('El precio es de S/. 2.00.');
+                    break;
+                case 'pepino':
+                    alert('El precio es de 6 x 8 soles.');
+                    break;
+                case 'tomate':
+                    alert('El precio es de S/. 3.50.');
+                    break;
+                case 'choclo':
+                    alert('El precio es de 7 x 14 soles.');
+                    break;
+                case 'zanahoria':
+                alert('El precio es de S/. 2.50.');
+                break;
+                default:    
+                    alert('Tipo de verdura desconocido.');
+                    break;
+            }
+            }
